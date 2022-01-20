284aa1af50a397da0cc0ab95957a55761b6af0d7
Greyson Newton <greynewt@tamu.edu>
2022-01-20 10:37:09 -0600

    Update pycryptobot.py

--- pycryptobot.py
+++ pycryptobot.py
@@ -1305,10 +1305,11 @@ def executeJob(
                                 _state.fib_high = bands[second_key]
 
                     else:
+                        text_box.center(str(current_sim_date))
                         text_box.singleLine()
                         text_box.center("*** Executing TEST Buy Order ***")
                         text_box.singleLine()
-                        text_box.center(str(current_sim_date))
+                        
                     _app.trade_tracker = _app.trade_tracker.append(
                         {
                             "Datetime": str(current_sim_date),
